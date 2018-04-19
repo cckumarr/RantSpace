bc7f6b0ec3708ce7fc3e7eb2533294221d993c09
chandan <chandan.punyakumar@pearson.com>
2018-04-18 21:50:26 -0600

    missed files bcos of new ide

---
+++ src/client/app/mock-rants.ts
@@ -0,0 +1,14 @@
+import { Rants } from './rants';
+
+export const RANTS: Rants[] = [
+  { id: 11, likes: 1, rantMsg: 'this is a nice little rant.' },
+  { id: 12, likes: 2, rantMsg: 'this is a nice little rant but what happens is i rant more and more and more like crazy amounts and it gets to be a pretty big rant.' },
+  { id: 13, likes: 3, rantMsg: 'Bombasto' },
+  { id: 14, likes: 4, rantMsg: 'Celeritas' },
+  { id: 15, likes: 5, rantMsg: 'Magneta' },
+  { id: 16, likes: 16, rantMsg: 'RubberMan' },
+  { id: 17, likes: 7, rantMsg: 'Dynama' },
+  { id: 18, likes: 8, rantMsg: 'Dr IQ' },
+  { id: 19, likes: 9, rantMsg: 'Magma' },
+  { id: 20, likes: 10, rantMsg: 'Tornado' }
+];
---
+++ src/client/app/rants.ts
@@ -0,0 +1,5 @@
+export class Rants {
+  id: number;
+  likes: number;
+  rantMsg: string;
+}
