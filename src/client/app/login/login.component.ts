e006a30602a4902b2c5f6c8d160e7c3b94216a63
chandan <chandan.punyakumar@pearson.com>
2018-04-18 20:09:47 -0600

    changes with center and mock rants

--- src/client/app/login/login.component.ts
+++ src/client/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Rants } from '../rants';
+import { RANTS } from '../mock-rants';
 
 /**
 *	This class represents the lazy loaded LoginComponent.
@@ -11,4 +13,16 @@ import { Component } from '@angular/core';
 	styleUrls: ['login.css']
 })
 
-export class LoginComponent { }
+export class LoginComponent {
+number = 6;
+rants = RANTS;
+}
+
+// angular.module('controllerAsExample', []).controller('SettingsController1', function($scope) {
+//
+//   $scope.contacts = []
+//   $scope.addContact = function() {
+//     $scope.contacts.push({
+//     })
+//   }
+// });
